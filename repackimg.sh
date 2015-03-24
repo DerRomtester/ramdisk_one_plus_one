eaedabca18ce6dc3b0333827686b45b323c6b4cc
DerRomtester <dieb.stefan.96@gmail.com>
2015-03-24 17:43:47 +0100

    ramdisk: update my tools and squash 24.03.2015 ramdisk cm nightly
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- repackimg.sh
+++ repackimg.sh
@@ -10,26 +10,32 @@ chmod -R 755 "$bin" "$PWD"/*.sh;
 chmod 644 "$bin/magic";
 cd "$PWD";
 
+arch=`uname -m`;
+
 if [ -z "$(ls split_img/* 2> /dev/null)" -o -z "$(ls ramdisk/* 2> /dev/null)" ]; then
   echo "No files found to be packed/built.";
   abort;
-  return 1;
+  exit 1;
 fi;
 
-echo "\nAndroid Image Kitchen - RepackImg Script";
-echo "by osm0sis @ xda-developers\n";
+clear;
+echo " ";
+echo "Android Image Kitchen - RepackImg Script";
+echo "by osm0sis @ xda-developers";
+echo " ";
 
 if [ ! -z "$(ls *-new.* 2> /dev/null)" ]; then
-  echo "Warning: Overwriting existing files!\n";
+  echo "Warning: Overwriting existing files!";
+  echo " ";
 fi;
 
 rm -f ramdisk-new.cpio*;
 case $args in
-  -o|--original)
-    args=-o;
+  --original)
     echo "Repacking with original ramdisk...";;
   *)
-    echo "Packing ramdisk...\n";
+    echo "Packing ramdisk...";
+    echo " ";
     ramdiskcomp=`cat split_img/*-ramdiskcomp`;
     echo "Using compression: $ramdiskcomp";
     repackcmd="$ramdiskcomp";
@@ -40,27 +46,29 @@ case $args in
       xz) repackcmd="xz -1 -Ccrc32";;
       lzma) repackcmd="xz -Flzma";;
       bzip2) compext=bz2;;
-      lz4) repackcmd="$bin/lz4 -l -9 -f stdin stdout";;
+      lz4) repackcmd="$bin/$arch/lz4 -l stdin stdout";;
     esac;
     cd ramdisk;
-    find . | cpio -o -H newc | $repackcmd > ../ramdisk-new.cpio.$compext;
-    if [ $? -eq "1" ]; then
+    find . | cpio -H newc -o 2> /dev/null | $repackcmd > ../ramdisk-new.cpio.$compext;
+    if [ ! $? -eq "0" ]; then
       abort;
-      return 1;
+      exit 1;
     fi;
     cd ..;;
 esac;
 
-echo "\nGetting build information...\n";
+echo " ";
+echo "Getting build information...";
 cd split_img;
 kernel=`ls *-zImage`;               echo "kernel = $kernel";
-if [ "$args" = "-o" ]; then
+if [ "$args" = "--original" ]; then
   ramdisk=`ls *-ramdisk.cpio*`;     echo "ramdisk = $ramdisk";
   ramdisk="split_img/$ramdisk";
 else
   ramdisk="ramdisk-new.cpio.$compext";
 fi;
 cmdline=`cat *-cmdline`;            echo "cmdline = $cmdline";
+board=`cat *-board`;                echo "board = $board";
 base=`cat *-base`;                  echo "base = $base";
 pagesize=`cat *-pagesize`;          echo "pagesize = $pagesize";
 kerneloff=`cat *-kerneloff`;        echo "kernel_offset = $kerneloff";
@@ -78,13 +86,15 @@ if [ -f *-dtb ]; then
 fi;
 cd ..;
 
-echo "\nBuilding image...\n"
-$bin/mkbootimg --kernel "split_img/$kernel" --ramdisk "$ramdisk" $second --cmdline "$cmdline" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb -o image-new.img;
-if [ $? -eq "1" ]; then
+echo " ";
+echo "Building image...";
+echo " ";
+$bin/$arch/mkbootimg --kernel "split_img/$kernel" --ramdisk "$ramdisk" $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb -o image-new.img;
+if [ ! $? -eq "0" ]; then
   abort;
-  return 1;
+  exit 1;
 fi;
 
 echo "Done!";
-return 0;
+exit 0;
 
